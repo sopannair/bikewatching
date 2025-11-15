a86c8d87cedc547ed63cd27a4ad5eb5f5d077cff
sopannair <sopan.nair@gmail.com>
2025-11-14 22:43:46 -0800

    added map files

---
+++ map.js
@@ -0,0 +1,18 @@
+// Import Mapbox as an ESM module
+import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+
+// Check that Mapbox GL JS is loaded
+console.log('Mapbox GL JS Loaded:', mapboxgl);
+
+// Set your Mapbox access token here
+mapboxgl.accessToken = 'pk.eyJ1Ijoic29wYW5uYWlyIiwiYSI6ImNtaHp4MXY0YzBlMHIyam16dHY0bDdjM3oifQ.HkB_T3u53W4EGoOriuw1tw';
+
+// Initialize the map
+const map = new mapboxgl.Map({
+  container: 'map', // ID of the div where the map will render
+  style: 'mapbox://styles/mapbox/streets-v12', // Map style
+  center: [-71.09415, 42.36027], // [longitude, latitude]
+  zoom: 12, // Initial zoom level
+  minZoom: 5, // Minimum allowed zoom
+  maxZoom: 18, // Maximum allowed zoom
+});
